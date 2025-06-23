468e394cf2ed430d9ff944497ddd43724d349348
ClothingDonations <hdmcwebsiteproject@gmail.com>
2025-06-23 11:00:35 +0530

    update(3.0)

--- suggest-location.js
+++ suggest-location.js
@@ -1,16 +1,6 @@
 const deliveryLocations = [
-  { name: "Location 1", lat: 15.4580, lon: 75.0078 },
-  { name: "Location 2", lat: 15.3694, lon: 75.1239 },
-  { name: "Location 3", lat: 15.3647, lon: 75.1239 },
-  { name: "Location 4", lat: 15.39288, lon: 75.0815 },
-  { name: "Location 5", lat: 15.3639, lon: 75.1145 },
-  { name: "Location 6", lat: 15.3653, lon: 75.1245 },
-  { name: "Location 7", lat: 15.3494, lon: 75.1103 },
-  { name: "Location 8", lat: 15.3518, lon: 75.1147 },
-  { name: "Location 9", lat: 15.3325, lon: 75.1251 },
-  { name: "Location 10", lat: 15.3497, lon: 75.1334 },
-  { name: "Location 11", lat: 15.3647, lon: 75.1239 },
-  { name: "Location 12", lat: 15.4603, lon: 75.0103 }
+  { name: "Location 1", lat: 15.35163 , lon: 75.14054  },
+  { name: "Location 2", lat: 15.350217, lon: 75.137625 }
 ];
 
 const addressInput = document.querySelector("textarea[name='address']");
